d1900dfdad8639902eb5f459592fca8f904bfa68
curlvin <macshild@gmail.com>
2020-03-18 20:32:48 -0500

    made changes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+/*it('renders without crashing', () => {
   /*const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div); */
+  ReactDOM.unmountComponentAtNode(div); 
 
   //switiching to dummy test
-});
+}); */
+
+it('renders without crashing', () => {}); 
